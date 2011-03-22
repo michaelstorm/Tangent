6d9b0f4cdf9b340e89861a493bd5bc0ac38ba3a3
Michael Storm <mstorm7@gmail.com>
2011-03-22 04:45:56 -0500

    Added draw_radius and draw_point to circle drawer.

--- circle.c
+++ circle.c
@@ -3,18 +3,19 @@
 #include "chord.h"
 #include "circle.h"
 
-struct circle *new_circle(int diam, int dens)
+struct circle *new_circle(int diam, int dens, long double rot)
 {
-	struct circle *c = (struct circle *)malloc(sizeof(struct circle));
+	struct circle *c = malloc(sizeof(struct circle));
 	c->diameter = diam;
 	c->row_count = c->diameter*ROW_RATIO;
 	c->density = dens;
+	c->rotation = rot;
 
-	c->rows = (char **)malloc(sizeof(char *)*c->row_count);
+	c->rows = malloc(sizeof(char *)*c->row_count);
 
 	int i, j;
 	for (i = 0; i < c->row_count; i++) {
-		c->rows[i] = (char *)malloc(sizeof(char)*c->diameter);
+		c->rows[i] = malloc(sizeof(char)*c->diameter);
 		for (j = 0; j < c->diameter; j++)
 			c->rows[i][j] = ' ';
 	}
@@ -32,22 +33,128 @@ void free_circle(struct circle *c)
 	free(c);
 }
 
-void draw_circle(struct circle *c, char marker, long double from_rad,
-				   long double to_rad)
+void draw_circle(struct circle *c, char marker)
 {
-	int i;
-	int points = c->diameter*c->density;
+	draw_arc(c, marker, 0, 2*PI);
+}
+
+long double get_x(struct circle *c, long double rad)
+{
+	return cos(rad)*(c->diameter-1)/2 + c->diameter/2;
+}
+
+long double get_y(struct circle *c, long double rad)
+{
+	return sin(rad)*(c->diameter-1)/2 + c->diameter/2;
+}
+
+void draw_arc(struct circle *c, char marker, long double from_rad,
+			  long double to_rad)
+{
+	from_rad += c->rotation;
+	to_rad += c->rotation;
+
+	int points = PI*c->diameter*c->density;
 	long double rad_range = to_rad > from_rad ? to_rad - from_rad
 											  : 2*PI - (from_rad-to_rad);
 
+	int i;
 	for (i = 0; i < points; i++) {
-		long double rad = rad_range*i/points+from_rad-PI/2;
-		long double x = cos(rad)*(c->diameter-1)/2 + c->diameter/2;
-		long double y = sin(rad)*(c->diameter-1)/2 + c->diameter/2;
-		int row = (int)(y*ROW_RATIO);
-		int col = (int)x;
+		long double rad = rad_range*i/points+from_rad;
+		int row = (int)(get_y(c, rad)*ROW_RATIO);
+		int col = (int)get_x(c, rad);
 		c->rows[row][col] = marker;
 	}
+}
+
+long double normalize_radians(long double rad)
+{
+	// wrap in if-blocks to prevent infinite loop in weird rounding cases
+	if (rad > 2*PI) {
+		while (rad > 2*PI)
+			rad -= 2*PI;
+	}
+	else if (rad < 0) {
+		while (rad < 0)
+			rad += 2*PI;
+	}
+	return rad;
+}
+
+void draw_radius_automarker(struct circle *c, long double to_rad)
+{
+	char marker;
+	long double norm_rad = normalize_radians(to_rad + c->rotation);
+
+	if (norm_rad < PI) {
+		if (norm_rad < PI/8)
+			marker = '-';
+		else if (norm_rad < 3*PI/8)
+			marker = '\\';
+		else if (norm_rad < 5*PI/8)
+			marker = '|';
+		else if (norm_rad < 7*PI/8)
+			marker = '/';
+		else
+			marker = '-';
+	}
+	else {
+		if (norm_rad < 9*PI/8)
+			marker = '-';
+		else if (norm_rad < 11*PI/8)
+			marker = '\\';
+		else if (norm_rad < 13*PI/8)
+			marker = '|';
+		else if (norm_rad < 15*PI/8)
+			marker = '/';
+		else
+			marker = '-';
+	}
+
+	draw_radius(c, marker, to_rad);
+}
+
+void draw_radius(struct circle *c, char marker, long double to_rad)
+{
+	to_rad += c->rotation;
+	long double radius = ((long double)c->diameter)/2;
+	long double domain = cos(to_rad)*radius;
+	long double slope = sin(to_rad)/cos(to_rad);
+	int points = radius*c->density;
+
+	if (!isfinite(slope)) {
+		to_rad = normalize_radians(to_rad);
+		int mult = 1;
+		if (fabs(to_rad - 3*PI/2) < fabs(to_rad - PI/2))
+			mult = -1;
+
+		int i;
+		int col = radius;
+		for (i = 0; i < points; i++) {
+			long double y = radius+((long double)i)/points*radius*mult;
+			int row = y*ROW_RATIO;
+			c->rows[row][col] = marker;
+		}
+	}
+	else {
+		int i;
+		for (i = 0; i < points; i++) {
+			long double x = radius+((long double)i)/points*domain;
+			long double y = radius+((long double)i)/points*domain*slope;
+
+			int col = x;
+			int row = y*ROW_RATIO;
+			c->rows[row][col] = marker;
+		}
+	}
+}
+
+void draw_point(struct circle *c, char marker, long double rad)
+{
+	rad += c->rotation;
+	int row = (int)(get_y(c, rad)*ROW_RATIO);
+	int col = (int)get_x(c, rad);
+	c->rows[row][col] = marker;
 }
 
 void print_circle(FILE *fp, struct circle *c)
@@ -68,7 +175,7 @@ long double id_to_radians(const chordID *id)
 	long double rad = 0.0;
 
 	for (i = 0; i < CHORD_ID_LEN; i++) {
-		long double numerator = ((long)id->x[i]);
+		long double numerator = id->x[i];
 		long double denominator = pow((long double)256, (long double)(i+1));
 		rad += numerator/denominator;
 	}
--- circle.h
+++ circle.h
@@ -2,7 +2,7 @@
 #define CHORD_CIRCLE_H
 
 #ifndef PI
-#define PI 3.1415926535
+#define PI 3.141592653589793238462643383279
 #endif
 
 #ifndef ROW_RATIO
@@ -14,13 +14,18 @@ struct circle
 	int diameter;
 	int row_count;
 	int density;
+	long double rotation;
 	char **rows;
 };
 
-struct circle *new_circle(int diam, int dens);
+struct circle *new_circle(int diam, int dens, long double rot);
 void free_circle(struct circle *c);
-void draw_circle(struct circle *c, char marker, long double from_rad,
-				   long double to_rad);
+void draw_circle(struct circle *c, char marker);
+void draw_arc(struct circle *c, char marker, long double from_rad,
+			  long double to_rad);
+void draw_radius_automarker(struct circle *c, long double to_rad);
+void draw_radius(struct circle *c, char marker, long double to_rad);
+void draw_point(struct circle *c, char marker, long double rad);
 void print_circle(FILE *fp, struct circle *c);
 
 long double id_to_radians(const chordID *id);
